b1ae1fa65abed1728cdb2be006ff193b536cbcb0
ObuchiYuki <yukibochi1@gmail.com>
2025-01-09 21:42:58 +0900

    init

---
+++ Sources/InteractiveTabView/Example/BottomBarScreen.swift
@@ -0,0 +1,81 @@
+//
+//  TabItem.swift
+//  InteractiveTabView
+//
+//  Created by yuki on 2025/01/09.
+//
+
+
+//
+//  TopBarScreen.swift
+//  InteractiveTabView
+//
+//  Created by yuki on 2025/01/09.
+//
+
+import SwiftUI
+
+fileprivate struct TabItem: Identifiable {
+    let id: Int
+    let title: String
+}
+
+fileprivate let tabItems = [
+    TabItem(id: 0, title: "Recommend"),
+    TabItem(id: 1, title: "Following"),
+    TabItem(id: 2, title: "Popular"),
+    TabItem(id: 3, title: "New"),
+    TabItem(id: 4, title: "Trend"),
+]
+
+struct TopBarScreen: View {
+    @State var selectedID: Int? = 0
+    @State var interaction: InteractiveTabViewInteraction? = nil
+    
+    var body: some View {
+        InteractiveTabView(
+            selection: self.$selectedID,
+            tabs: tabItems,
+            content: { item in
+                ScrollView {
+                    VStack {
+                        ForEach(0..<40, id: \.self) { i in
+                            Text("Item \(i)")
+                                .padding()
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                }
+            }
+        )
+        .onInteractionChange {
+            self.interaction = $0
+        }
+        .safeAreaInset(edge: .top) {
+            VStack(spacing: 0) {
+                InteractiveTabBar(
+                    selection: self.$selectedID,
+                    interaction: self.interaction,
+                    spacing: 8,
+                    tabs: tabItems,
+                    content: { item in
+                        Text(item.title)
+                            .font(.headline)
+                            .foregroundColor(
+                                item.id == self.selectedID ? Color.primary : Color.primary.opacity(0.25)
+                            )
+                            .padding(.vertical)
+                    }
+                )
+                .background(.ultraThinMaterial)
+                
+                Divider()
+            }
+        }
+        .animation(.easeInOut, value: self.selectedID)
+    }
+}
+
+#Preview {
+    TopBarScreen()
+}
---
+++ Sources/InteractiveTabView/InteractiveTabBar.swift
@@ -0,0 +1,146 @@
+//
+//  TabButtonFrameKey.swift
+//  TabLayoutSample
+//
+//  Created by yuki on 2025/01/09.
+//
+
+import SwiftUI
+
+fileprivate struct TabButtonFrameKey<ID: Hashable>: PreferenceKey {
+    static var defaultValue: [ID: Anchor<CGRect>] { [:] }
+
+    static func reduce(value: inout [ID: Anchor<CGRect>], nextValue: () -> [ID: Anchor<CGRect>]) {
+        value.merge(nextValue()) { $1 }
+    }
+}
+
+public struct InteractiveTabBar<TabItem: Identifiable, Content: View>: View {
+    @Binding var selection: TabItem.ID?
+
+    let tabs: [TabItem]
+
+    let content: (TabItem) -> Content
+
+    let interaction: InteractiveTabViewInteraction?
+
+    public init(
+        selection: Binding<TabItem.ID?>,
+        interaction: InteractiveTabViewInteraction?,
+        tabs: [TabItem],
+        @ViewBuilder content: @escaping (TabItem) -> Content
+    ) {
+        self._selection = selection
+        self.interaction = interaction
+        self.tabs = tabs
+        self.content = content
+    }
+
+    public var body: some View {
+        ScrollViewReader { proxy in
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 0) {
+                    ForEach(self.tabs) { tab in
+                        Button(action: { self.selection = tab.id }) {
+                            self.content(tab)
+                                .padding(.horizontal, 8)
+                        }
+                        .background(
+                            GeometryReader { proxy in
+                                Color.clear.anchorPreference(key: TabButtonFrameKey<TabItem.ID>.self, value: .bounds) {
+                                    [tab.id: $0]
+                                }
+                            }
+                        )
+                        .padding(.horizontal, 8)
+                    }
+                }
+            }
+            .onChange(of: self.selection, { _, newTabId in
+                if let newTabId,
+                   let index = self.tabs.firstIndex(where: { $0.id == newTabId }) {
+                    withAnimation(.easeInOut) {
+                        proxy.scrollTo(
+                            newTabId,
+                            anchor: UnitPoint(x: CGFloat(index) / CGFloat(self.tabs.count), y: 0)
+                        )
+                    }
+                }
+            })
+        }
+        .overlayPreferenceValue(TabButtonFrameKey.self) { value in
+            GeometryReader { proxy in
+                if let (indicatorX, indicatorWidth) = self.indicatorPositionAndWidth(in: proxy, anchors: value) {
+                    Capsule()
+                        .fill(Color.accentColor)
+                        .frame(width: indicatorWidth, height: 3)
+                        .position(x: indicatorX, y: proxy.size.height - 1.5)
+                } else {
+                    Text("Error!")
+                }
+            }
+        }
+    }
+
+    private func indicatorPositionAndWidth(
+        in proxy: GeometryProxy,
+        anchors: [TabItem.ID : Anchor<CGRect>]
+    ) -> (CGFloat, CGFloat)? {
+        
+        guard let interaction = self.interaction else {
+            return nil
+        }
+        
+        let safeCurrent = max(0, min(self.tabs.count - 1, interaction.currentIndex))
+        let safeNext   = max(0, min(self.tabs.count - 1, interaction.nextIndex))
+
+        guard let currentTabRect = anchors[self.tabs[safeCurrent].id].map({ proxy[$0] }),
+              let nextTabRect = anchors[self.tabs[safeNext].id].map({ proxy[$0] })
+        else { return nil }
+
+        let interpolatedMidX = self.lerp(a: currentTabRect.midX, b: nextTabRect.midX, t: interaction.fraction)
+        let interpolatedWidth = self.lerp(a: currentTabRect.width, b: nextTabRect.width, t: interaction.fraction)
+
+        return (interpolatedMidX, interpolatedWidth)
+    }
+
+    private func lerp(a: CGFloat, b: CGFloat, t: CGFloat) -> CGFloat {
+        a + (b - a) * t
+    }
+}
+
+
+fileprivate struct Tab: Identifiable {
+    let id: Int
+    let title: String
+    let color: Color
+}
+
+#Preview {
+    @Previewable @State var selectedID: Int? = 0
+    @Previewable @State var interaction: InteractiveTabViewInteraction = InteractiveTabViewInteraction(
+        currentIndex: 0,
+        nextIndex: 1,
+        fraction: 0.3
+    )
+    
+    let tabs = [
+        Tab(id: 0, title: "Tab 1", color: .red),
+        Tab(id: 1, title: "Tab 2", color: .green),
+        Tab(id: 2, title: "Tab 3", color: .blue),
+        Tab(id: 3, title: "Tab 4", color: .yellow),
+        Tab(id: 4, title: "Tab 5", color: .orange),
+        Tab(id: 5, title: "Tab 6", color: .purple),
+    ]
+    
+    
+    InteractiveTabBar(
+        selection: $selectedID,
+        interaction: interaction,
+        tabs: tabs,
+        content: { tab in
+            Text(tab.title)
+                .padding()
+        }
+    )
+}
